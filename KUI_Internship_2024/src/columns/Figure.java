55e1b5326e0928846c03ccfe10a3c2980af2158b
justzuka <kenchuashvili14@gmail.com>
2024-09-10 04:33:09 +0400

    columns -> desktop

--- KUI_Internship_2024/src/columns/Figure.java
+++ KUI_Internship_2024/src/columns/Figure.java
@@ -1,20 +1,18 @@
 package columns;
-import java.applet.*;
-import java.awt.*;
-import java.util.*;
 
+import java.util.Random;
 
 class Figure {
-	static int x=Columns.Width/2+1, y=1, c[]=new int[4];
+	int x, y;
+	int c[] = new int[4];
 	static Random r = new Random();
 
-	Figure()
-	{
-		x = Columns.Width/2+1;
+	Figure() {
+		x = Columns.Width / 2 + 1;
 		y = 1;
 		c[0] = 0;
-		c[1] = (int)(Math.abs(r.nextInt())%7+1);
-		c[2] = (int)(Math.abs(r.nextInt())%7+1);
-		c[3] = (int)(Math.abs(r.nextInt())%7+1);
+		c[1] = (int) (Math.abs(r.nextInt()) % 7 + 1);
+		c[2] = (int) (Math.abs(r.nextInt()) % 7 + 1);
+		c[3] = (int) (Math.abs(r.nextInt()) % 7 + 1);
 	}
 }
